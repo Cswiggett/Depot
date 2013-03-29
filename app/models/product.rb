02f422971645d5d3c5b044b6999ad35ce94746eb
cswiggett <cs@streeteasy.com>
2013-03-29 18:06:09 -0400

    validation plus

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, :price, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :length => { :minimum => 10 }
   validates :title, :uniqueness => true
   validates :image_url, :format => {
     :with   => %r{\.(gif|jpg|png)$}i,
