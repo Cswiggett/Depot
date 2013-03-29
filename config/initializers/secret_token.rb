c5847033e5f94f9cafbcb31d26d1676e76543a1a
cswiggett <cs@streeteasy.com>
2013-03-28 22:28:53 -0400

    second try

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '6fe450bce91734157c899a81aab0d5949a8eb02dd5e1c4208ca72dd855cf3ce004fc1bf5a7a8846aa037fda44eb4be41c9f4195ecc142cb734b2092a587ac27b'
