dade8de7d175ceba539de8c4db7db59711bc1457
cswiggett <cs@streeteasy.com>
2013-03-28 22:59:12 -0400

    third try

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130328034726) do
+ActiveRecord::Schema.define(:version => 20130329023748) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
-    t.string   "description"
+    t.text     "description"
     t.string   "image_url"
     t.decimal  "price",       :precision => 10, :scale => 0
     t.datetime "created_at",                                 :null => false
