16d38476c627afa1778d322b1ed4c74b84d0a24b
cswiggett <cs@streeteasy.com>
2013-04-22 08:34:17 -0400

    added cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
 	default_scope :order => 'title'
+  has_many :line_items
+  before_destroy :ensure_not_refereneced_by_any_line_item
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, :price, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
@@ -9,4 +11,12 @@ class Product < ActiveRecord::Base
     :with   => %r{\.(gif|jpg|png)$}i,
     :message => 'Broke ass, make this a URL for a GIF, JPG, or PNG.'
   }
+  def ensure_not_referenced_by_any_line_item
+    if line_items.count.zero?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
 end
